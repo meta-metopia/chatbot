67ef3b8ee3ceb8e7a23f77bd464d78b53f4e5b1b
sirily11 <sirily1997@gmail.com>
2023-02-05 13:30:39 +0800

    fix: update configure

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -4,13 +4,12 @@ import Vapor
 
 // configures your application
 public func configure(_ app: Application) async throws {
-    try await setupWebhook(app: app)
+//    try await setupWebhook(app: app)
     try app.databases.use(.mongo(
         connectionString: Environment.get("DATABASE_URL")!
     ), as: .mongo)
 
     app.migrations.add(CreateSessions())
-
     // register routes
     try routes(app)
 }
